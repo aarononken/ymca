c2d1b0d4261f7acb01bd7459205e50feb84940ec
Aaron Onken <aarononken@gmail.com>
2014-11-19 01:06:14 +0000

    Set checks#new as root

---
+++ app/controllers/checks_controller.rb
@@ -0,0 +1,3 @@
+class ChecksController < ApplicationController
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   resources :memperships
   resources :programs
   
+  root 'checks#new'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
