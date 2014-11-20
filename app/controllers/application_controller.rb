063971c0a57b8c5aca830ee3bee4df58d7236a01
Aaron Onken <aarononken@gmail.com>
2014-11-20 17:44:07 +0000

    Adding redirect to families#index after admin sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(admin)
     families_path
   end
+  
+  def after_sign_up_path_for(admin)
+    families_path
+  end
 end
