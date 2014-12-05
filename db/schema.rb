9f16f57c50b0d353de147567559940fa72694e4c
Michelle Vankat <michelle@interfaceschool.com>
2014-12-05 15:38:28 +0000

    Trying to pull and told me I had to commit first?

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203015301) do
+ActiveRecord::Schema.define(version: 20141201164054) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -44,12 +44,30 @@ ActiveRecord::Schema.define(version: 20141203015301) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
 
+  create_table "admins", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
+  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+
   create_table "checks", force: true do |t|
     t.integer  "family_id"
     t.integer  "child_id"
-    t.boolean  "checked_in", default: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.boolean  "checked_in"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "program_id"
   end
 
@@ -73,10 +91,11 @@ ActiveRecord::Schema.define(version: 20141203015301) do
   end
 
   create_table "memberships", force: true do |t|
+    t.integer  "membership_id"
     t.integer  "child_id"
     t.integer  "program_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "programs", force: true do |t|
