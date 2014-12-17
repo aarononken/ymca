35cf6fd9db2d05eae96af5160572c7c06f903ebf
Aaron Onken <aarononken@gmail.com>
2014-12-17 02:15:41 +0000

    Customizing filter for membership#index

--- app/admin/membership.rb
+++ app/admin/membership.rb
@@ -25,4 +25,8 @@ ActiveAdmin.register Membership do
     end
     actions
   end
+  
+  filter :child_child_first_name, :as => :string, :label => "First Name"
+  filter :child_child_last_name, :as => :string, :label => "Last Name"
+  filter :program_program_name, :as => :select, :collection => Program.all.map(&:program_name), :label => "Program Name"
 end
